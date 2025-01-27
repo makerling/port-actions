c4787bf79f1f2efd463bc1163ee511099c734ce7
Stevan Vanderwerf <github@vanderling.com>
2025-01-27 12:35:37 -0600

    moving folder

--- terraform-for-sonarqube-server/output.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip_address" {
-  description = "The public IP address of the virtual machine"
-  value       = azurerm_public_ip.publicip.ip_address
-}
-
-output "vm_name" {
-  description = "The name of the virtual machine"
-  value       = azurerm_linux_virtual_machine.main.name
-}
