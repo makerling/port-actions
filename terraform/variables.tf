1c2ddcddb2d3c3d270ddc30de0834772d426c0d7
Stevan Vanderwerf <github@vanderling.com>
2025-01-27 19:45:55 -0600

    adding port run id to variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,23 @@ variable "admin_password" {
   type        = string
   sensitive   = true
 }
+
+variable "port_run_id" {
+    type        = string
+    description = "The runID of the action run that created the entity"
+}
+
+variable "port_client_id" {
+    type        = string
+    description = "The Port client ID"
+}
+
+variable "port_client_secret" {
+    type        = string
+    description = "The Port client secret"
+}
+
+variable "base_url" {
+    type        = string
+    description = "The Port API URL"
+}
