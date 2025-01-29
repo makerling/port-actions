ea1c9f595fd04cd08d47dbb9a9472ff13a5d4111
Stevan Vanderwerf <github@vanderling.com>
2025-01-29 08:19:27 -0600

    fixed syntax error

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "vm_subnet_${var.vm_name_prefix}" "vm_subnet"
+  name                 = "vm_subnet_${var.vm_name_prefix}"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
